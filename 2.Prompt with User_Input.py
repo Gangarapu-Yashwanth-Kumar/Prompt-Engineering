b67d3dc39132d9c4e2cc94345819ffbda6370b2f
Yashwanth Kumar <yashwanthkumar6666@gmail.com>
2025-08-13 16:21:22 +0530

    Prompt Engineering

---
+++ 2.Prompt with User_Input.py
@@ -0,0 +1,18 @@
+#Creating Prompts Based on User Input...
+
+# Example of creating a prompt based on User Input
+def create_prompt():
+    topic = input("Enter the Topic for the Story: ")
+    tone = input("Enter the Tone (Ex: , Serious, Humorous, Adventurous, Futuristic): ")
+    
+    prompt = f"Write a {tone} story about {topic}."
+    return prompt
+
+# Generate prompt based on user input
+user_prompt = create_prompt()
+print("Generated Prompt:", user_prompt)
+
+'''
+f-string: The f before the string indicates that it’s an f-string, 
+which allows you to embed expressions inside the string. 
+The expressions are enclosed in curly braces {}. ''' 
